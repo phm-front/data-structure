d1633b70efd1a0af6903d128d57b4d4dedd1be9a
phm-front <506129591@qq.com>
2023-04-17 17:46:31 +0800

    冒泡排序优化

--- algorithm/sort/bubbleSort.ts
+++ algorithm/sort/bubbleSort.ts
@@ -1,3 +1,4 @@
+import { measureSort } from "hy-algokit";
 import { testSort } from "../utils/sortTest";
 
 // 冒泡排序
@@ -6,16 +7,20 @@ function bubbleSort(arr: number[]) {
   // 循环次数为len - 1 - i 因为是两个值进行比较，所以最后一个值不需要进行比较
   for (let i = 0; i < len - 1; i++) {
     // 减i是因为前i次循环已经将最大的数放到了最后 不需要进行比较
+    let isSwap = false;
     for (let j = 0; j < len - 1 - i; j++) {
       // 如果前一个数比后一个数大 则交换位置
       if (arr[j] > arr[j + 1]) {
         // 解构赋值交换位置
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        isSwap = true;
       }
     }
+    if (!isSwap) break;
   }
   return arr;
 }
 
 // 测试
 testSort(bubbleSort)
+measureSort(bubbleSort, 10000)
