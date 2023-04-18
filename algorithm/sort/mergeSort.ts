a52665e83fa7c4b52e4da7224ef132dd46823333
phm-front <506129591@qq.com>
2023-04-18 14:14:29 +0800

    归并排序

---
+++ algorithm/sort/mergeSort.ts
@@ -0,0 +1,31 @@
+import { measureSort } from "hy-algokit";
+import { testSort } from "../utils/sortTest";
+
+// 归并排序
+function mergeSort(arr: number[]): number[] {
+  if (arr.length <= 1) return arr;
+  // 分解
+  const mid = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, mid);
+  const rightArr = arr.slice(mid);
+  // 合并
+  return mergeSortedArr(mergeSort(leftArr), mergeSort(rightArr));
+}
+// 合并两个有序数组
+function mergeSortedArr(a: number[], b: number[]) {
+  let i = 0, j = 0;
+  // 声明新数组
+  const mergeArr: number[] = []
+  while(i < a.length && j < b.length) {
+    // 将较小的元素放入新数组
+    a[i] < b[j] ? mergeArr.push(a[i++]) : mergeArr.push(b[j++]);
+  }
+  // 处理剩余元素
+  if (i < a.length) mergeArr.push(...a.slice(i));
+  if (j < b.length) mergeArr.push(...b.slice(j));
+  return mergeArr;
+}
+
+// 测试
+testSort(mergeSort)
+measureSort(mergeSort, 10000)
