a8da112a240e91abff6217f42408e52fc8b50643
phm-front <506129591@qq.com>
2023-05-04 17:22:31 +0800

    测试脚本命令

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// console.log(process.env)
+console.log(process.env.npm_package_name);
+console.log(process.env.npm_package_version);
+console.log(process.env.npm_package_config_key);
+// 获取脚本命令参数
+console.log(process.argv);
